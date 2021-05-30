3b83051de023e1b2e508a9610f7aaaba2e4e72ea
kong <type5617@163.com>
2021-05-30 13:17:43 +0800

    制作WikiHeader组件并使用

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        v-model:selectedKeys="selectedKeys1"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    
+    <WikiHeader></WikiHeader>
+
     <router-view />
     <a-layout-footer style="text-align: center">
       wiki开放知识库系统 ©2021 Created by love kong company
     </a-layout-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WikiHeader from "@/components/WikiHeader.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "App",
+  components: {
+    WikiHeader
+  },
+});
+</script>
 
 <style>
 .logo {
